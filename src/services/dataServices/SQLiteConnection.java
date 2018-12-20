baa86988afb454b999326de126c76c89d4153568
damien tohin <damientohin@gmail.com>
2018-12-20 12:44:27 +0100

    Added jdbc connection and disconnection code

--- src/services/dataServices/SQLiteConnection.java
+++ src/services/dataServices/SQLiteConnection.java
@@ -17,13 +17,28 @@ public class SQLiteConnection {
     private SQLiteConnection() {
     }
 
-    public void Connect(){
+    public boolean Connect(){
         try {
             String url = "jdbc:sqlite:C:\\Users\\dtohi\\Desktop\\Epitech\\Cash Manager\\CashManagerDB.db";
             connection = DriverManager.getConnection(url);
             System.out.println("Connection to database terminated");
+            return true;
         } catch (SQLException e){
             System.out.println("SQLite connection failed");
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    public boolean DisConnect(){
+        try {
+            connection.close();
+            System.out.println("SQlite connection closed");
+            return true;
+        } catch (SQLException e) {
+            System.out.println("SQLite Connection Could not be closed.");
+            e.printStackTrace();
+            return false;
         }
     }
 }
